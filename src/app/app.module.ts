49b52e6408028d6b4d6c0cc9a5e2d2ee4978be14
Alfredo <alfredo_alejandro@live.com>
2021-05-21 15:53:37 -0500

    Master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './alerts/warningalert/warningalert.component';
+import { SuccessalertComponent } from './alerts/successalert/successalert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]', <--Selector cuando es un atributo de un div
+  // selector: '.app-servers', <--Selector cuando es una clase css de un div
+  selector: 'app-servers', // <--Selector normal
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+allowNewServer = false;
+serverCreationStatus = 'No server can be created'
+serverName = 'TestServer';
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+  }
+}
