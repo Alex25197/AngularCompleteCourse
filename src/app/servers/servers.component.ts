d401ebbffe0a49b0a69c50ded99bb0d53a58657a
Alfredo Pinzon <alfredo_alejandro@live.com>
2021-05-23 19:16:28 -0500

    utilizacion de ngFor

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,6 +11,8 @@ export class ServersComponent implements OnInit {
 allowNewServer = false;
 serverCreationStatus = 'No server can be created'
 serverName = 'TestServer';
+serverCreated = false;
+servers = ['Testserver', 'Testserver 2'];
   constructor() { 
     setTimeout(() => {
       this.allowNewServer = true;
@@ -21,6 +23,8 @@ serverName = 'TestServer';
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
