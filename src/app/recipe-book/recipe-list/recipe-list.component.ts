5a42e5f2fde1bccbdc545eedab253ce6560ddb0b
Alfredo <alfredo_alejandro@live.com>
2021-06-17 19:37:48 -0500

    new changes

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test recipe',
+      'Simple Test',
+      'https://cdn.pixabay.com/photo/2018/04/16/15/56/food-3325036_960_720.jpg'
+    ),
+    new Recipe(
+      'Test recipe',
+      'Simple Test',
+      'https://cdn.pixabay.com/photo/2018/04/16/15/56/food-3325036_960_720.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/shared/models/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  //esto es una forma abreviada de definir el modelo, lo que recibe como parametro en el constructor son los atributos del modelo
+  constructor(public name: string, public amount: number) {}
+}
