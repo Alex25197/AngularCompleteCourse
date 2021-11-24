6d76c40a6cfa9793769df6536c729873aab1b34b
Alfredo Pinzon <apinzon@hypernovalabs.com>
2021-11-24 14:14:26 -0500

    add

--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,7 +1,10 @@
 
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/models/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -10,16 +13,30 @@ export class RecipeService {
           'Chorizo & mozzarella gnocchi bake',
           'Test',
           'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?webp=true&quality=90&resize=440%2C400'
-        ),
+          ,[
+            new Ingredient('Meat', 1),
+            new Ingredient('French Frieds', 20)
+          ]
+          ),
         new Recipe(
           'Thai fried prawn & pineapple rice',
           'Test',
           'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/thai-aea8468.jpg?webp=true&quality=90&resize=440%2C400'
-        ),
+        ,[
+          new Ingredient('Buns', 2),
+          new Ingredient('Meat', 2)
+        ]),
       ];
+
+      constructor(private slService: ShoppingListService){}
 
       getRecipes(){
           //Se pone slice para que lo que retorne, sea una copia de la propiedad guardada en el servicio, no el mismo arreglo
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
+
       }
 }
